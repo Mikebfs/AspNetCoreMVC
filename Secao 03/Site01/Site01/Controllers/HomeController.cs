f300e36dc4c08120afef37bbf3904c71c4bab648
Maicon Batista Farias da Silva <maiconbfsilva@hotmail.com>
2019-04-12 15:30:21 -0300

    Asp.Net core até aula 17

---
+++ Secao 03/Site01/Site01/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace Site01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return new ContentResult() {Content= "Olá mundo", ContentType="application/json", StatusCode = 200 }; 
+
+            return View();
+        }
+    }
+}
